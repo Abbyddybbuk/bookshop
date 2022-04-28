a47c4dc793da92ffd95d84bb15704276d37d7f38
Abhijeet Kulshreshtha <abhijeet.abap@gmail.com>
2022-04-28 13:05:09 +0100

    Changes

---
+++ src/test/java/com/sap/dev/bookshop/BookshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.dev.bookshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
